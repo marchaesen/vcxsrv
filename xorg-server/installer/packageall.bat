3f64595b7c8d6b8389e85476bb6a089ed6415426
Marc Haesen <Marc.Haesen@oneaccess-net.com>
2015-10-19 17:05:11 +0200

    Temporary disable the installation of the VC run-time files since they completely have been changed in Visual Studio 2015

--- xorg-server/installer/packageall.bat
+++ xorg-server/installer/packageall.bat
@@ -3,10 +3,10 @@ if exist vcxsrv*.installer.exe del vcxsrv*.installer.exe
 
 if "%1"=="nox86" goto skipx86
 
-copy "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\redist\x86\Microsoft.VC120.CRT\msvcp120.dll"
-copy "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\redist\x86\Microsoft.VC120.CRT\msvcr120.dll"
-copy "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\redist\Debug_NonRedist\x86\Microsoft.VC120.DebugCRT\msvcp120d.dll"
-copy "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\redist\Debug_NonRedist\x86\Microsoft.VC120.DebugCRT\msvcr120d.dll"
+rem copy "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x86\Microsoft.VC140.CRT\msvcp140.dll"
+rem copy "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x86\Microsoft.VC140.CRT\msvcr140.dll"
+rem copy "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\Debug_NonRedist\x86\Microsoft.VC140.DebugCRT\msvcp140d.dll"
+rem copy "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\Debug_NonRedist\x86\Microsoft.VC140.DebugCRT\msvcr140d.dll"
 
 if exist "C:\Program Files (x86)\NSIS\makensis.exe" (
   "C:\Program Files (x86)\NSIS\makensis.exe" vcxsrv.nsi
@@ -19,10 +19,10 @@ if exist "C:\Program Files (x86)\NSIS\makensis.exe" (
 :skipx86
 if "%1"=="nox64" goto skipx64
 
-copy "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\redist\x64\Microsoft.VC120.CRT\msvcp120.dll"
-copy "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\redist\x64\Microsoft.VC120.CRT\msvcr120.dll"
-copy "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\redist\Debug_NonRedist\x64\Microsoft.VC120.DebugCRT\msvcp120d.dll"
-copy "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\redist\Debug_NonRedist\x64\Microsoft.VC120.DebugCRT\msvcr120d.dll"
+rem copy "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x64\Microsoft.VC140.CRT\msvcp140.dll"
+rem copy "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x64\Microsoft.VC140.CRT\msvcr140.dll"
+rem copy "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\Debug_NonRedist\x64\Microsoft.VC140.DebugCRT\msvcp140d.dll"
+rem copy "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\Debug_NonRedist\x64\Microsoft.VC140.DebugCRT\msvcr140d.dll"
 
 if exist "C:\Program Files (x86)\NSIS\makensis.exe" (
   "C:\Program Files (x86)\NSIS\makensis.exe" vcxsrv-64.nsi
@@ -34,7 +34,7 @@ if exist "C:\Program Files (x86)\NSIS\makensis.exe" (
 
 :skipx64
 
-del msvcr120.dll
-del msvcr120d.dll
-del msvcp120.dll
-del msvcp120d.dll
+rem del msvcr140.dll
+rem del msvcr140d.dll
+rem del msvcp140.dll
+rem del msvcp140d.dll
