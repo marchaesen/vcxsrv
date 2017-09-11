0655ee721e33fb00fd8e45870e55a7d40686d9e0
marha <marha@users.sourceforge.net>
2017-09-11 12:05:27 +0200

    Release 1.19.3.1

--- include/dix-config.h
+++ include/dix-config.h
@@ -382,7 +382,7 @@
 #undef XORG_RELEASE
 
 /* Current Xorg version */
-#define XORG_VERSION_CURRENT (((1) * 10000000) + ((19) * 100000) + ((3) * 1000) + 0)
+#define XORG_VERSION_CURRENT (((1) * 10000000) + ((19) * 100000) + ((3) * 1000) + 1)
 
 /* Xorg release date */
 #define XORG_DATE "16 November 2015"
