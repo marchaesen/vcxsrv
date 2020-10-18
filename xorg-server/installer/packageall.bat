e0866865e6df70273ad10bb8ddb09694de5a7df0
marha <marha@users.sourceforge.net>
2020-10-18 19:52:19 +0200

    Upgraded to latest version of Visual Studio 2019

--- xorg-server/installer/packageall.bat
+++ xorg-server/installer/packageall.bat
@@ -4,10 +4,10 @@ if "%1"=="nox86" goto skipx86
 
 if exist vcxsrv.*.installer.exe del vcxsrv.*.installer.exe
 
-copy "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Redist\MSVC\14.25.28508\x86\Microsoft.VC142.CRT\msvcp140.dll"
-copy "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Redist\MSVC\14.25.28508\x86\Microsoft.VC142.CRT\vcruntime140.dll"
-copy "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Redist\MSVC\14.25.28508\debug_nonredist\x86\Microsoft.VC142.DebugCRT\msvcp140d.dll"
-copy "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Redist\MSVC\14.25.28508\debug_nonredist\x86\Microsoft.VC142.DebugCRT\vcruntime140d.dll"
+copy "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Redist\MSVC\14.27.29016\x86\Microsoft.VC142.CRT\msvcp140.dll"
+copy "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Redist\MSVC\14.27.29016\x86\Microsoft.VC142.CRT\vcruntime140.dll"
+copy "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Redist\MSVC\14.27.29016\debug_nonredist\x86\Microsoft.VC142.DebugCRT\msvcp140d.dll"
+copy "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Redist\MSVC\14.27.29016\debug_nonredist\x86\Microsoft.VC142.DebugCRT\vcruntime140d.dll"
 
 if exist "C:\Program Files (x86)\NSIS\makensis.exe" (
   "C:\Program Files (x86)\NSIS\makensis.exe" vcxsrv.nsi
@@ -22,12 +22,12 @@ if "%1"=="nox64" goto skipx64
 
 if exist vcxsrv-64.*.installer.exe del vcxsrv-64.*.installer.exe
 
-copy "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Redist\MSVC\14.25.28508\x64\Microsoft.VC142.CRT\msvcp140.dll"
-copy "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Redist\MSVC\14.25.28508\x64\Microsoft.VC142.CRT\vcruntime140.dll"
-copy "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Redist\MSVC\14.25.28508\x64\Microsoft.VC142.CRT\vcruntime140_1.dll"
-copy "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Redist\MSVC\14.25.28508\debug_nonredist\x64\Microsoft.VC142.DebugCRT\msvcp140d.dll"
-copy "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Redist\MSVC\14.25.28508\debug_nonredist\x64\Microsoft.VC142.DebugCRT\vcruntime140d.dll"
-copy "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Redist\MSVC\14.25.28508\debug_nonredist\x64\Microsoft.VC142.DebugCRT\vcruntime140_1d.dll"
+copy "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Redist\MSVC\14.27.29016\x64\Microsoft.VC142.CRT\msvcp140.dll"
+copy "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Redist\MSVC\14.27.29016\x64\Microsoft.VC142.CRT\vcruntime140.dll"
+copy "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Redist\MSVC\14.27.29016\x64\Microsoft.VC142.CRT\vcruntime140_1.dll"
+copy "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Redist\MSVC\14.27.29016\debug_nonredist\x64\Microsoft.VC142.DebugCRT\msvcp140d.dll"
+copy "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Redist\MSVC\14.27.29016\debug_nonredist\x64\Microsoft.VC142.DebugCRT\vcruntime140d.dll"
+copy "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Redist\MSVC\14.27.29016\debug_nonredist\x64\Microsoft.VC142.DebugCRT\vcruntime140_1d.dll"
 
 if exist "C:\Program Files (x86)\NSIS\makensis.exe" (
   "C:\Program Files (x86)\NSIS\makensis.exe" vcxsrv-64.nsi
