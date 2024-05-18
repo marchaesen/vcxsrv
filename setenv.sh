e1e44445d4b8f631fb935241069ff2cc44ae224d
Marc Haesen <marchaesen@users.noreply.github.com>
2024-05-18 17:50:10 +0200

    Solved some 32-bit compilation problems.

--- setenv.sh
+++ setenv.sh
@@ -6,7 +6,11 @@ rm -f commands.sh
 python setenv.py $1 > commands.sh
 chmod +x commands.sh
 source commands.sh
+if [[ "$1" == "1" ]] ; then
 export PATH=/usr/local/bin:/usr/local/sbin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/wsl/lib:$DIR/tools/mhmake/Release64:/mnt/c/nasm:$PATH:/mnt/c/gnuwin32/bin:/mnt/c/perl/perl/bin
+else
+export PATH=/usr/local/bin:/usr/local/sbin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/wsl/lib:$DIR/tools/mhmake/Release:/mnt/c/nasm:$PATH:/mnt/c/gnuwin32/bin:/mnt/c/perl/perl/bin
+fi
 rm -f commands.sh
 export MHMAKECONF=$DIR
 export PYTHON3=/mnt/c/Python39/python.exe
